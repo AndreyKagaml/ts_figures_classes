abd29c48b15d2165905bf05319366bf79f7977ff
Andrey <kagamlykas@gmail.com>
2026-02-02 00:54:19 +0200

    Solved task

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,69 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  public shape: string = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (b + c <= a || a + c <= b || a + b <= c) {
+      throw new Error('Incorrect triangle sides');
+    }
+  }
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const s = 0.5 * (this.a + this.b + this.c);
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return (
+      Math.trunc(
+        Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)) * 100,
+      ) / 100
+    );
+  }
+}
+
+export class Circle implements Figure {
+  public shape: string = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('Incorrect value of radius');
+    }
+  }
+
+  getArea(): number {
+    return Math.trunc(Math.PI * this.radius ** 2 * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: string = 'rectangle';
+
+  constructor(
+    public color: string,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('Incorrect value of sides');
+    }
+  }
+
+  getArea(): number {
+    return Math.trunc(this.width * this.height * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
